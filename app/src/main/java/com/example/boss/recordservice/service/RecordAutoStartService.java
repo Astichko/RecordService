1a029791db7e0d34765f0b6799a1fbabf439ba06
Eugen <jenia_ja@mail.ru>
2016-12-01 00:56:46 +0200

    Display widget

--- app/src/main/java/com/example/boss/recordservice/service/RecordAutoStartService.java
+++ app/src/main/java/com/example/boss/recordservice/service/RecordAutoStartService.java
@@ -2,11 +2,20 @@ package com.example.boss.recordservice.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.boss.recordservice.R;
 import com.example.boss.recordservice.utils.AppStatsCollectHelper;
 
 /**
@@ -15,14 +24,25 @@ import com.example.boss.recordservice.utils.AppStatsCollectHelper;
 
 public class RecordAutoStartService extends Service {
 
-    private AppStatsCollectHelper appStats;
     final private int FIRST_START_DELAY = 0;
     final private int INTERVAL_DELAY = 10000;//10 sec
+
+    private WindowManager windowManager;
+    private View floatingView;
+    private WindowManager.LayoutParams params;
+    private AppStatsCollectHelper appStats;
+    private boolean isWidgetVisible = false;
+    private boolean isMove;
+
+    @Override
+    public void onCreate() {
+        initAppStatsCollectHelper();
+        initWidget();
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(getBaseContext(), "Service started", Toast.LENGTH_SHORT).show();
-        initAppStatsCollectHelper();
         startMonitoring();
         super.onStartCommand(intent, flags, startId);
         return START_STICKY;
@@ -58,7 +78,7 @@ public class RecordAutoStartService extends Service {
         String currentPackageName = appStats.getCurrentAppPackageName();
         for (String str : appStats.getAppsTargetName()) {
             if (str.equals(currentPackageName)) {//If Match to requested app
-                displayMatch();
+                displayWidget();
             }
         }
     }
@@ -71,5 +91,90 @@ public class RecordAutoStartService extends Service {
         Intent intent = new Intent(this, ServiceDialog.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    public void displayWidget() {
+        if (!isWidgetVisible) {
+            windowManager.addView(floatingView, params);
+            isWidgetVisible = true;
+        }
+    }
+
+    public void initWidget() {
+        initView();
+        setWidgetListeners();
+    }
+
+    public void initView() {
+        floatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+        params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+    }
+
+    public void setWidgetListeners() {
+        ImageView closeButton = (ImageView) floatingView.findViewById(R.id.imageClose);
+        final ImageView startButton = (ImageView) floatingView.findViewById(R.id.imageStartRecord);
+        final ImageView stopButton = (ImageView) floatingView.findViewById(R.id.imageStopRecord);
+        final FrameLayout frameLayout = (FrameLayout) floatingView.findViewById(R.id.frameLay);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeWidget();
+            }
+        });
+        frameLayout.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+                        //get the touch location
+                        initialTouchX = motionEvent.getRawX();
+                        initialTouchY = motionEvent.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        isMove = true;
+                        params.x = initialX + (int) (motionEvent.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (motionEvent.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(floatingView, params);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (!isMove) {
+                            if (stopButton.getVisibility() == View.GONE) {
+                                stopButton.setVisibility(View.VISIBLE);
+                                startButton.setVisibility(View.GONE);
+                            } else {
+                                startButton.setVisibility(View.VISIBLE);
+                                stopButton.setVisibility(View.GONE);
+                            }
+                        }
+                        isMove = false;
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void removeWidget() {
+        windowManager.removeView(floatingView);
     }
 }
